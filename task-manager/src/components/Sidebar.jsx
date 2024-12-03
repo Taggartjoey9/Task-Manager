85ec805e75dae034096c55bfae544b6efb06fcdc
Taggartjoey9 <taggartjoey9@gmail.com>
2024-12-02 20:16:56 -0500

    configuring side nav bar with styling component setup

---
+++ task-manager/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+
+export default function Sidebar() {
+    return (
+        <section className="side-container">
+            <div className="side-content">
+                <h1>Your Projects</h1>
+                <button>+ Add Project</button>
+            </div>
+        </section>
+    )
+}
