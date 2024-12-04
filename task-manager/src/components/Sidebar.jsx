7df26d5a50dc4906c895e1dbced9ad97c867bcdd
Taggartjoey9 <taggartjoey9@gmail.com>
2024-12-04 17:37:23 -0500

    adjusting styling for sidebar

--- task-manager/src/components/Sidebar.jsx
+++ task-manager/src/components/Sidebar.jsx
@@ -5,6 +5,7 @@ export default function Sidebar() {
             <div className="side-content">
                 <h1>Your Projects</h1>
                 <button>+ Add Project</button>
+                <ul></ul>
             </div>
         </section>
     )
